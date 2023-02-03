e485a18843633a5c115cf1a1a6508723b68d2358
imberbus <imberbusg@gmail.com>
2023-02-03 23:31:51 +0100

    SpawnPoint & Root prefab. RootManager is a singleton

--- GlobalGameJam/Assets/src/RootsManager.cs
+++ GlobalGameJam/Assets/src/RootsManager.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace src
@@ -10,22 +9,41 @@ namespace src
         [SerializeField] private float minRootRange;
         [SerializeField] private float maxRootRange;
         [SerializeField] private GameObject spawnPointContainer;
+        [SerializeField] private GameObject rootContainer;
+
         private float currentTimer = 0f;
+        public static RootsManager _instance { get; private set; }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
 
         // Start is called before the first frame update
         void Start()
         {
-        
         }
 
         // Update is called once per frame
 
         public void SpawnRoot()
         {
-            var availableSpawnPoints = spawnPointContainer.GetComponentsInChildren<Transform>();
+            var availableSpawnPoints = spawnPointContainer.GetComponentsInChildren<SpawnPoint>();
             var selectedSpawnPoint = availableSpawnPoints[Random.Range(0, availableSpawnPoints.Length)];
-            Instantiate(rootPrefab, selectedSpawnPoint.position, selectedSpawnPoint.rotation);
+            var selectedTransform = selectedSpawnPoint.transform;
+            var instantiatedRoot = Instantiate(rootPrefab, selectedTransform.position,
+                selectedTransform.rotation, rootContainer.transform);
+            
+            instantiatedRoot.transform.LookAt(selectedSpawnPoint.getRandomNeighbour());
         }
+
         void Update()
         {
             currentTimer += Time.deltaTime;
@@ -34,6 +52,15 @@ namespace src
                 currentTimer = 0f;
                 SpawnRoot();
             }
+        }
+
+        public float GetMinRootRange()
+        {
+            return minRootRange;
+        }
+        public float GetMaxRootRange()
+        {
+            return maxRootRange;
         }
     }
 }
---
+++ GlobalGameJam/Assets/src/SpawnPoint.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace src
+{
+    public class SpawnPoint : MonoBehaviour
+    {
+        private List<Transform> _eligibleNeighbours; 
+        void Start()
+        {
+            _eligibleNeighbours = new List<Transform>();
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, RootsManager._instance.GetMaxRootRange() - RootsManager._instance.GetMinRootRange());
+
+            foreach (Collider collider in hitColliders)
+            {
+                Debug.Log(collider.gameObject.name + " is within range.");
+                _eligibleNeighbours.Add(collider.gameObject.transform);
+            }
+        }
+
+        public Transform getRandomNeighbour()
+        {
+            return _eligibleNeighbours[Random.Range(0, _eligibleNeighbours.Count)];
+        }
+    }
+}
