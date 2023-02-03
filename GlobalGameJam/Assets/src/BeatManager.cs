c0f2b1241a9e3645619887426567221ce1b65950
Strublar <daluzmatteo@gmail.com>
2023-02-03 21:56:13 +0100

    BeatManager

--- GlobalGameJam/Assets/src/BeatManager.cs
+++ GlobalGameJam/Assets/src/BeatManager.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BeatManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static UnityEvent Beat = new UnityEvent();
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float beatPeriod;
+    private float currentTimer = 0f;
+    public void Update()
     {
-        
+        currentTimer += Time.deltaTime;
+        if (currentTimer >= beatPeriod)
+        {
+            currentTimer = 0f;
+            Beat.Invoke();
+        }
     }
 }
