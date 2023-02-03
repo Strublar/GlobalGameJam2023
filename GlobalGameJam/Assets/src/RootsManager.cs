15d409beed7fad3cb74e04707150870f127f6194
imberbus <imberbusg@gmail.com>
2023-02-03 22:37:03 +0100

    Root manager init root prefab init git ignore intelliJ setup files

--- GlobalGameJam/Assets/src/RootsManager.cs
+++ GlobalGameJam/Assets/src/RootsManager.cs
@@ -1,18 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RootsManager : MonoBehaviour
+namespace src
 {
-    // Start is called before the first frame update
-    void Start()
+    public class RootsManager : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private GameObject rootPrefab;
+        [SerializeField] private float spawnPeriod;
+        [SerializeField] private float minRootRange;
+        [SerializeField] private float maxRootRange;
+        [SerializeField] private GameObject spawnPointContainer;
+        private float currentTimer = 0f;
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
         
+        }
+
+        // Update is called once per frame
+
+        public void SpawnRoot()
+        {
+            var availableSpawnPoints = spawnPointContainer.GetComponentsInChildren<Transform>();
+            var selectedSpawnPoint = availableSpawnPoints[Random.Range(0, availableSpawnPoints.Length)];
+            Instantiate(rootPrefab, selectedSpawnPoint.position, selectedSpawnPoint.rotation);
+        }
+        void Update()
+        {
+            currentTimer += Time.deltaTime;
+            if (currentTimer >= spawnPeriod)
+            {
+                currentTimer = 0f;
+                SpawnRoot();
+            }
+        }
     }
 }
