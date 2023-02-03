3e31917af895ce9d7a633e0d12a0e49d638319ef
Strublar <daluzmatteo@gmail.com>
2023-02-04 00:04:29 +0100

    Beat acceleration

--- GlobalGameJam/Assets/src/BeatManager.cs
+++ GlobalGameJam/Assets/src/BeatManager.cs
@@ -3,28 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class BeatManager : MonoBehaviour
 {
     public static UnityEvent Beat = new UnityEvent();
 
-    [SerializeField] private float beatPeriod;
+    [SerializeField] private float initialBeatPeriod;
+    [SerializeField] private float beatMultiplierPerBeat;
+
+    private float currentBeatPeriod;
     private float currentTimer = 0f;
 
     public void Start()
     {
-        Beat.AddListener(PrintBeat);
+        currentBeatPeriod = initialBeatPeriod;
+        Beat.AddListener(UpdateBeat);
     }
 
-    public void PrintBeat()
+    public void UpdateBeat()
     {
         Debug.Log("Beat");
+        currentBeatPeriod *= beatMultiplierPerBeat;
     }
 
     public void Update()
     {
         currentTimer += Time.deltaTime;
-        if (currentTimer >= beatPeriod)
+        if (currentTimer >= currentBeatPeriod)
         {
             currentTimer = 0f;
             Beat.Invoke();
