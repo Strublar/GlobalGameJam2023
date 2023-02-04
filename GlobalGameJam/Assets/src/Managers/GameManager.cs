d4466af88e59e459411e30e29c3adb0c6eafd3d7
imberbus <imberbusg@gmail.com>
2023-02-04 14:29:46 +0100

    Camera shake proportionnally to the number of root cut by the dash

--- GlobalGameJam/Assets/src/Managers/GameManager.cs
+++ GlobalGameJam/Assets/src/Managers/GameManager.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [SerializeField] private GameObject cameraObj;
+    [SerializeField] private float shakeCoeff;
+    
+    
     
     private void Awake()
     {
@@ -20,14 +25,16 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         scoreTxt.text = _score.ToString();
+
     }
 
     public void IncrementScore(int scoreAmount)
     {
         _score += scoreAmount;
         scoreTxt.color = GetColorAccordingToScoreBoost(scoreAmount);
+        ShakeCameraAccordingToScoreIncrease(scoreAmount);
         scoreTxt.text = _score.ToString();
-        Debug.Log("current score :" + _score.ToString());
+        
     }
     private static Color GetColorAccordingToScoreBoost(int scoreAmount)
     {
@@ -41,7 +48,19 @@ public class GameManager : MonoBehaviour
             return Color.yellow;
         }
         return Color.red;
-    } 
+    }
+
+    private void ShakeCameraAccordingToScoreIncrease(int scoreAmount)
+    {
+        if (scoreAmount == 0)
+        {
+            return;
+        }
+        
+        var shakeScore = shakeCoeff * scoreAmount;
+        var shakeVector = new Vector3(shakeScore, shakeScore, shakeScore);
+        Camera.main.DOShakePosition(1, new Vector3(0.5f,0.5f,0.5f), 20, 45f, false, ShakeRandomnessMode.Harmonic);
+    }
 
     // Update is called once per frame
     void Update()
