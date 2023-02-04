8589d7deedd542f3b623f12ea5f7e5c25405060a
Strublar <daluzmatteo@gmail.com>
2023-02-04 12:38:12 +0100

    OffBeat Spawning period

--- GlobalGameJam/Assets/src/Managers/RootsManager.cs
+++ GlobalGameJam/Assets/src/Managers/RootsManager.cs
@@ -8,20 +8,21 @@ namespace src
     public class RootsManager : MonoBehaviour
     {
         public static RootsManager instance;
-        
-        [Header("Root behaviour")] 
-        public float expandDuration;
 
-        [Header("Root generation management")]
-        [SerializeField] private GameObject rootPrefab;
+        [Header("Root behaviour")] public float expandDuration;
+
+        [Header("Root generation management")] [SerializeField]
+        private GameObject rootPrefab;
+
         [SerializeField] private int baseSpawnPeriod;
         //[SerializeField] private  spawnPeriodMultiplierPerSpawn;
-        
+
         [SerializeField] private float minRootRange;
         [SerializeField] private float maxRootRange;
-        
-        [Header("Spawn point management")]
-        [SerializeField] private GameObject spawnPointContainer;
+
+        [Header("Spawn point management")] [SerializeField]
+        private GameObject spawnPointContainer;
+
         [SerializeField] private GameObject rootContainer;
 
         private int currentBeatCount;
@@ -31,38 +32,44 @@ namespace src
         {
             instance = this;
         }
-        
+
         public void SpawnSeed()
         {
             SpawnPoint[] availableSpawnPoints = spawnPointContainer.GetComponentsInChildren<SpawnPoint>();
             SpawnPoint selectedSpawnPoint = availableSpawnPoints[Random.Range(0, availableSpawnPoints.Length)];
-            SpawnRootAtTarget(selectedSpawnPoint,true);
-
+            SpawnRootAtTarget(selectedSpawnPoint, true);
         }
 
-        public void SpawnRootAtTarget(SpawnPoint selectedSpawnPoint,bool isSeed)
+        public void SpawnRootAtTarget(SpawnPoint selectedSpawnPoint, bool isSeed)
         {
             var selectedTransform = selectedSpawnPoint.transform;
             var instantiatedRoot = Instantiate(rootPrefab, selectedTransform.position,
                 selectedTransform.rotation, rootContainer.transform);
-            instantiatedRoot.GetComponent<Root>().Init(selectedSpawnPoint,selectedSpawnPoint.getRandomNeighbour(),this, isSeed);
+            instantiatedRoot.GetComponent<Root>()
+                .Init(selectedSpawnPoint, selectedSpawnPoint.getRandomNeighbour(), this, isSeed);
         }
 
         private void Start()
         {
             currentSpawnPeriod = baseSpawnPeriod;
-            BeatManager.OffBeat.AddListener(SpawnSeed);
+            BeatManager.OffBeat.AddListener(SpawnOffBeat);
         }
 
         private void SpawnOffBeat()
         {
-            
+            currentBeatCount++;
+            if (currentBeatCount >= currentSpawnPeriod)
+            {
+                currentBeatCount = 0;
+                SpawnSeed();
+            }
         }
 
         public float GetMinRootRange()
         {
             return minRootRange;
         }
+
         public float GetMaxRootRange()
         {
             return maxRootRange;
